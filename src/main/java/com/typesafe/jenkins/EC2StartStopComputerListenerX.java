e0a9d048dd3680440e7de1b0297745ae62c0d09e
Adriaan Moors <adriaan.moors@typesafe.com>
2015-01-06 13:52:40 -0800

    Put the annotation on a Java class. TODO: y no scala??

---
+++ src/main/java/com/typesafe/jenkins/EC2StartStopComputerListenerX.java
@@ -0,0 +1,4 @@
+package com.typesafe.jenkins;
+
+@hudson.Extension // TODO: the annotation doesn't work in scala...
+public class EC2StartStopComputerListenerX extends EC2StartStopComputerListener {}
--- src/main/scala/EC2StartStop.scala
+++ src/main/scala/EC2StartStop.scala
@@ -5,9 +5,6 @@ import scala.collection.JavaConverters._
 import hudson.model._
 import hudson.slaves._
 
-// TODO: extension is not being picked up...
-// go to $jenkins/script and run: (new com.typesafe.jenkins.EC2StartStopComputerListenerBase()).register()
-
 /**
  * A simple ComputerListener that launches an EC2 worker before a node is started, and stops the
  * worker when the Jenkins node is taken off-line.
@@ -18,7 +15,6 @@ import hudson.slaves._
  * If no tag prefixed by "ec2:" is found, the Jenkins node
  * name is used.
  */
-@hudson.Extension
 class EC2StartStopComputerListener extends ComputerListener {
   def ec2NodeNameFor(computer: Computer) =
     computer.getNode.getAssignedLabels.asScala.map(_.toString).filter(_.startsWith("ec2:")).map(_.drop(4)).headOption.getOrElse(computer.getName)
